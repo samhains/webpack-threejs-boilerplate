8aeb62535db95611aac4ec93c6815a2782e7345f
sam hains <sam@samhains.com>
2017-05-29 12:36:38 +1000

    cleaning up

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,13 @@
 import * as THREE from 'three';
-//import AbstractApplication from 'scripts/views/AbstractApplication';
 const glslify = require( 'glslify' );
 const bufferFrag = glslify( './../shaders/bufferA.frag' );
 const imageFrag = glslify( './../shaders/image.frag' );
 
-function Main()  {
+function Main() {
+
 	var scene;
 	var camera;
 	var renderer;
-
-	function sceneSetup() {
-		//This is the basic scene setup
-		scene = new THREE.Scene();
-		var width = window.innerWidth;
-		var height = window.innerHeight;
-		//Note that we're using an orthographic camera here rather than a prespective
-		camera = new THREE.OrthographicCamera(width/-2, width/2,  height/2, height/-2, -10000, 10000);
-
-		renderer = new THREE.WebGLRenderer();
-		renderer.setSize( width, height );
-		document.body.appendChild( renderer.domElement );
-	}
-
 	var bufferScene;
 	var textureA;
 	var textureB;
@@ -31,65 +17,78 @@ function Main()  {
 	var finalMaterial;
 	var quad;
 
-	function bufferTextureSetup(){
+	function sceneSetup() {
+
+		//This is the basic scene setup
+		scene = new THREE.Scene();
+		var width = window.innerWidth;
+		var height = window.innerHeight;
+		//Note that we're using an orthographic camera here rather than a prespective
+		camera = new THREE.OrthographicCamera( width / - 2, width / 2, height / 2, height / - 2, - 1, 1000 );
+
+		renderer = new THREE.WebGLRenderer();
+		renderer.setSize( width, height );
+		document.body.appendChild( renderer.domElement );
+		plane = new THREE.PlaneBufferGeometry( width, height );
+		bufferObject = new THREE.Mesh( plane, bufferMaterial );
+		bufferScene.add( bufferObject );
+		//Draw textureB to screen
+		quad = new THREE.Mesh( plane, finalMaterial );
+		scene.add( quad );
+
+	}
+
+	function bufferTextureSetup() {
+
+		let width = window.innerWidth;
+		let height = window.innerHeight;
+
 		let video = document.getElementById( 'video' );
 		let videoTexture = new THREE.VideoTexture( video );
 
 		videoTexture.minFilter = THREE.LinearFilter;
 		videoTexture.magFilter = THREE.LinearFilter;
 		videoTexture.format = THREE.RGBFormat;
-		//Create buffer scene
 		bufferScene = new THREE.Scene();
-		//Create 2 buffer textures
-		console.log(window.innerWidth, window.innerHeight);
-		textureA = new THREE.WebGLRenderTarget( window.innerWidth, window.innerHeight, { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter});
-		textureB = new THREE.WebGLRenderTarget( window.innerWidth, window.innerHeight, { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter} );
-		//Pass textureA to shader
+		textureA = new THREE.WebGLRenderTarget( width, height, { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter } );
+		textureB = new THREE.WebGLRenderTarget( width, height, { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter } );
 
 		bufferMaterial = new THREE.ShaderMaterial( {
 			uniforms: {
-				iChannel0: { type: "t", value: videoTexture },
-				iChannel1: { type: "t", value: textureA },
-				iResolution : {type: 'v2', value: new THREE.Vector2(window.innerWidth,window.innerHeight)},//Keeps the resolution
-				iGlobalTime: {type:"f",value:0.0}
+				iChannel0: { value: videoTexture },
+				iChannel1: { value: textureA },
+				iResolution: { value: new THREE.Vector2( width, height ) },
+				iGlobalTime: { value: 0.0 }
 			},
 			fragmentShader: bufferFrag
 		} );
 
-		plane = new THREE.PlaneBufferGeometry( window.innerWidth, window.innerHeight );
-		bufferObject = new THREE.Mesh( plane, bufferMaterial );
-		bufferScene.add(bufferObject);
-
 		finalMaterial = new THREE.ShaderMaterial( {
 			uniforms: {
-			 iChannel1: { type: "t", value: textureB },
-			 iResolution : {type: 'v2',value:new THREE.Vector2(window.innerWidth,window.innerHeight)},//Keeps the resolution
-			 iChannel0: { type: "t", value: videoTexture },
-			 iGlobalTime: {type:"f",value:0.0}
+			 iChannel1: { value: textureB },
+			 iResolution: { value: new THREE.Vector2( width, height ) },
+			 iChannel0: { value: videoTexture },
+			 iGlobalTime: { value: 0.0 }
 			},
 			fragmentShader: imageFrag
 		} );
-		//Draw textureB to screen 
-		quad = new THREE.Mesh( plane, finalMaterial );
-		scene.add(quad);
+
+
 	}
 
-	//Initialize the Threejs scene
-	sceneSetup();
 
-	//Setup the frame buffer/videoTexture we're going to be rendering to instead of the screen
 	bufferTextureSetup();
 
-	
+	sceneSetup();
 
-	//Render everything!
+
 	function render() {
 
 	  requestAnimationFrame( render );
 
 	  //Draw to textureB
 	  renderer.render( bufferScene, camera, textureB );
-		
+
 	  //Swap textureA and B
 	  var t = textureA;
 	  textureA = textureB;
@@ -104,10 +103,10 @@ function Main()  {
 
 	  //Finally, draw to the screen
 	  renderer.render( scene, camera );
+
 	}
+
 	render();
-
-
 
 
 }
--- src/shaders/bufferA.frag
+++ src/shaders/bufferA.frag
@@ -1,21 +1,86 @@
 import noise from 'glsl-noise/simplex/2d'
 
+vec2 offset [9];
+float kernel [9];
 uniform sampler2D iChannel0;
 uniform sampler2D iChannel1;
 uniform float iGlobalTime;
 uniform vec2 iResolution;
 
+//void main()
+//{
+    //vec2 res = iResolution.xy;
+    //vec2 tc = gl_FragCoord.xy / res;
+    //vec2 uv = tc;
+    
+    //uv *= 0.998;
+    
+    //vec4 sum = texture2D(iChannel1, uv);
+    //vec4 src = texture2D(iChannel0, tc);
+    
+    //sum.rgb = mix(sum.rbg, src.rgb, 0.04);
+    //gl_FragColor = sum;
+//}
+
+vec2 rotate(vec2 coords, float angle){
+	float sin_factor = sin(angle );
+    float cos_factor = cos(angle );
+    coords = vec2((coords.x - 0.5) , coords.y - 0.5) * mat2(cos_factor, sin_factor, -sin_factor, cos_factor);
+    coords += 0.5;
+    return coords;
+}
+
 void main()
 {
     vec2 res = iResolution.xy;
+    float t = iGlobalTime*0.01;
     vec2 tc = gl_FragCoord.xy / res;
-    vec2 uv = tc;
+    vec2 uv = -1.0 + 2.0 * tc;
     
-    uv *= 0.998;
+    //zoom 
+    uv *= 0.995;
+    uv = uv * 0.5 + 0.5;
+    
+    //rotation
+    uv = rotate(uv, 0.0015);
+    
+    vec2 step = 1.0 / res;
+    
+    offset[0] = vec2(-step.x, -step.y);
+    offset[1] = vec2(0.0, -step.y);
+    offset[2] = vec2(step.x, -step.y);
+    
+    offset[3] = vec2(-step.x, 0.0);
+    offset[4] = vec2(0.0, 0.0);
+    offset[5] = vec2(step.x, 0.0);
+    
+    offset[6] = vec2(-step.x, step.y);
+    offset[7] = vec2(0.0, step.y);
+    offset[8] = vec2(step.x, step.y);
+    
+    //convolution values
+    float outer = -0.25;
+    float inner = 0.125;
+    
+    kernel[0] = 0.0; kernel[1] = 0.0; kernel[2] = inner;
+    kernel[3] = 0.0; kernel[4] = inner; kernel[5] = 0.0;
+    kernel[6] = outer; kernel[7] = 0.0; kernel[8] = 0.0;
     
     vec4 sum = texture2D(iChannel1, uv);
-    vec4 src = texture2D(iChannel0, tc);
+    vec4 last = sum;
+    last.gb = -1.0 + 2.0 * last.gb;
     
-    sum.rgb = mix(sum.rbg, src.rgb, 0.04);
-    gl_FragColor = sum;
+    for (int i = 0; i < 9; i++) {
+        vec4 color = texture2D(iChannel1, (uv + offset[i]) - last.gb*0.025);
+        sum += color * kernel[i];
+    }
+    
+    vec4 src = texture2D(iChannel0, tc);
+    sum.rgb = mix(sum.rgb, src.rgb, src.rgb*0.15);
+    
+    if(iGlobalTime < 1.0){
+        gl_FragColor = src;
+    } else {
+    	gl_FragColor = vec4(clamp(vec3(sum.rgb), vec3(0.0), vec3(1.0)), 1.0);
+    }
 }
